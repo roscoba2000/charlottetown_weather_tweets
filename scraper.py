5a5e59bf50ec22b919a45706d58ac02406f8c301
roscoba2000 <rosco.ba@gmail.com>
2015-07-08 20:53:04 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,22 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import requests
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import time
+import urllib
+import re
+import csv
+import tweepy
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+HTML = urllib.urlopen("http://www.theguardian.pe.ca/Weather-Forecast").read()
+HTML = re.search('<strong class="temperature">(.+?)</strong>', HTML, re.S)
+Temperature = HTML.group(1)
+
+TempTweet = "The temperature in Charlottetown is " + Temperature + " http://www.theguardian.pe.ca/Weather-Forecast"
+
+auth = tweepy.OAuthHandler("FownCzUifldM8XrpRN70t9Rpr", "tKZQDS1tzbXen2jHkPMhrdBzagDt62LC3JgESm2K4s7QdMdu0n")
+auth.set_access_token("9056452-PnygMdcxjnRohFEq2NmUeiSojgwIG9uEjyHJPyZMsQ", "zeAZBSf4VrbyewmrF5mbZV4GddW7y1FRgw4CATn8pG33r")
+api = tweepy.API(auth)
+
+# searchResult = api.search(geocode="43.6363,-79.353,500km", rpp="100")
+api.update_status(status=TempTweet)
+print TempTweet
