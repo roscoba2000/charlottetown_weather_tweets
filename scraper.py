7219f369790d69ca4c15011b39898c26f76c6996
roscoba2000 <rosco.ba@gmail.com>
2015-07-10 22:49:04 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,12 +8,16 @@ HTML = urllib.urlopen("http://www.theguardian.pe.ca/Weather-Forecast").read()
 HTML = re.search('<strong class="temperature">(.+?)</strong>', HTML, re.S)
 Temperature = HTML.group(1)
 
+ErrorValue = "a"
+
 TempTweet = "The temperature in Charlottetown is " + Temperature + " http://www.theguardian.pe.ca/Weather-Forecast"
 
-auth = tweepy.OAuthHandler("o5NHBNd94C8MHOwKfmRW6Am36", "J2va7YswmudkDXC3CuXdjN6WjAlZYnyQUTdYTiZqcGd1Dqmgpj")
-auth.set_access_token("3351764193-OJibbL4vL61nnzeKEf9EJEx0gOf5v0pxBfmQGJG", "FlkMDrcAi1iTgE94CppjiwVoZNMXpKiSTYAfJbXQ4Iime")
-api = tweepy.API(auth)
-
-# searchResult = api.search(geocode="43.6363,-79.353,500km", rpp="100")
-api.update_status(status=TempTweet)
-print TempTweet
+while Temperature != ErrorValue:
+  try:
+	  auth = tweepy.OAuthHandler("o5NHBNd94C8MHOwKfmRW6Am36", "J2va7YswmudkDXC3CuXdjN6WjAlZYnyQUTdYTiZqcGd1Dqmgpj")
+	  auth.set_access_token("3351764193-OJibbL4vL61nnzeKEf9EJEx0gOf5v0pxBfmQGJG", "FlkMDrcAi1iTgE94CppjiwVoZNMXpKiSTYAfJbXQ4Iime")
+	  api = tweepy.API(auth)
+	  api.update_status(status=TempTweet)
+  except:
+	  pass
+  time.sleep(3600)
